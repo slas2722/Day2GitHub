86eaddeddd0ca07f3143d420d244f612d920b40e
slas2722 <140902956+slas2722@users.noreply.github.com>
2023-07-30 08:44:41 -0400

    Update DemoHomeWork.java

--- src/Day2HW/DemoHomeWork.java
+++ src/Day2HW/DemoHomeWork.java
@@ -3,6 +3,10 @@ package Day2HW;
 public class DemoHomeWork {
 	public static void main(String[] args) {
 		System.out.println("HomeWork");
+
+
+		public static void add( int a , int b ){
+			System.out.println( a + b );
 		
 	}
 
